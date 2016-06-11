5f5d714765792ced3f43988ae8cb3bfd444b3fc1
Shawn Allen <shawn.allen@gsa.gov>
2016-06-11 09:55:00 -0700

    add tests for all tab suites

--- test/specs/tab.js
+++ test/specs/tab.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const TabPage = require('../pageobjects/tab.page');
 
 const TAB = '[role=tab]';
 const TABPANEL = '[role=tabpanel]';
@@ -8,43 +9,107 @@ const TABINDEX = 'tabindex';
 
 describe('roles.tab.setup()', function() {
 
-  it('sets up tabs', function() {
-    browser.url('/tab.html');
+  var contexts = [
+    {root: '#li-roles'},
+    {root: '#button-roles', tabAt: 'li:nth-child(%) > [role=tab]'},
+    {root: '#dl', tabAt: '[role=tab]:nth-of-type(%)', panelAt: '[role=tabpanel]:nth-of-type(%)'}
+  ];
 
-    // initial conditions
-    assert.deepEqual(browser.getAttribute(TAB, EXPANDED),
-                     ['true', null, null]);
-    assert.deepEqual(browser.getAttribute(TABPANEL, HIDDEN),
-                     [null, null, null]);
-    assert.deepEqual(browser.getAttribute(TABPANEL, EXPANDED),
-                     [null, null, null]);
-    assert.deepEqual(browser.getAttribute(TABPANEL, TABINDEX),
-                     [null, null, null]);
+  contexts.forEach(function(ctx) {
 
-    browser.execute(function(selector) {
-      [].forEach.call(
-        document.querySelectorAll(selector),
-        function(tab, i) {
-          ariaTools.roles.tab.setup(tab);
-          tab.setAttribute('data-index', i);
-        }
-      );
-    }, TAB);
+    describe('context: ' + ctx.root, function() {
 
-    // ensure that the script ran
-    assert.deepEqual(browser.getAttribute(TAB, 'data-index'),
-                     ['0', '1', '2']);
+      var context = TabPage.getContext(ctx.root, ctx);
 
-    // after setup
-    assert.deepEqual(browser.getAttribute(TAB, EXPANDED),
-                     ['true', 'false', 'false']);
-    assert.deepEqual(browser.getAttribute(TABPANEL, HIDDEN),
-                     ['false', 'true', 'true']);
-    assert.deepEqual(browser.getAttribute(TABPANEL, EXPANDED),
-                     ['true', 'false', 'false']);
-    assert.deepEqual(browser.getAttribute(TABPANEL, TABINDEX),
-                     ['0', '-1', '-1']);
+      it('sets up tabs', function() {
 
-  });
+        TabPage.load();
+
+        // initial conditions
+        assert.deepEqual(browser.getAttribute(context.tabs, EXPANDED),
+                         ['true', null, null]);
+        assert.deepEqual(browser.getAttribute(context.panels, HIDDEN),
+                         [null, null, null]);
+        assert.deepEqual(browser.getAttribute(context.panels, EXPANDED),
+                         [null, null, null]);
+        assert.deepEqual(browser.getAttribute(context.panels, TABINDEX),
+                         [null, null, null]);
+
+        TabPage.setup();
+
+        // after setup
+        assert.deepEqual(browser.getAttribute(context.tabs, EXPANDED),
+                         ['true', 'false', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, HIDDEN),
+                         ['false', 'true', 'true']);
+        assert.deepEqual(browser.getAttribute(context.panels, EXPANDED),
+                         ['true', 'false', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, TABINDEX),
+                         ['0', '-1', '-1']);
+
+      });
+
+      it('toggles on click', function() {
+
+        TabPage.load();
+        TabPage.setup();
+
+        var tab = context.getTabAt(2);
+        browser.click(tab);
+
+        assert.deepEqual(browser.getAttribute(context.tabs, EXPANDED),
+                         ['true', 'true', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, HIDDEN),
+                         ['false', 'false', 'true']);
+        assert.deepEqual(browser.getAttribute(context.panels, EXPANDED),
+                         ['true', 'true', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, TABINDEX),
+                         ['0', '0', '-1']);
+
+        browser.click(tab);
+
+        assert.deepEqual(browser.getAttribute(context.tabs, EXPANDED),
+                         ['true', 'false', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, HIDDEN),
+                         ['false', 'true', 'true']);
+        assert.deepEqual(browser.getAttribute(context.panels, EXPANDED),
+                         ['true', 'false', 'false']);
+        assert.deepEqual(browser.getAttribute(context.panels, TABINDEX),
+                         ['0', '-1', '-1']);
+
+      });
+
+      it('can be torn down', function() {
+
+        TabPage.load();
+
+        var tab = context.getTabAt(1);
+        var panel = context.getPanelAt(1);
+
+        TabPage.setup();
+
+        assert.deepEqual(browser.getAttribute(tab, EXPANDED), 'true');
+        assert.deepEqual(browser.getAttribute(panel, HIDDEN), 'false');
+        assert.deepEqual(browser.getAttribute(panel, EXPANDED), 'true');
+        assert.deepEqual(browser.getAttribute(panel, TABINDEX), '0');
+
+        TabPage.teardown();
+
+        // three times should do it
+        browser.click(tab);
+        browser.click(tab);
+        browser.click(tab);
+
+        assert.deepEqual(browser.getAttribute(tab, EXPANDED), 'true');
+        assert.deepEqual(browser.getAttribute(panel, HIDDEN), 'false');
+        assert.deepEqual(browser.getAttribute(panel, EXPANDED), 'true');
+        assert.deepEqual(browser.getAttribute(panel, TABINDEX), '0');
+
+      });
+
+    });
+
+  }, this);
+
 
 });
